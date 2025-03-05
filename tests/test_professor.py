1c7fdc1132e35ae8f32b6ec8f62d34bd93984192
mercybabayemi <mercyjanet013@gmail.com>
2025-03-05 05:06:03 +0100

    complete student management, main in __init__, professor, and user with its implementation

--- tests/test_professor.py
+++ tests/test_professor.py
@@ -24,25 +24,25 @@ class TestProfessor(unittest.TestCase):
         self.assertIn("Mathematics", self.professor1.courses.courses)
 
     # def test_add_course_duplicate(self):
-    #     self.professor.add_course("Physics")
-    #     result = self.professor.add_course("Physics")
+    #     self.professor1.add_course("Physics")
+    #     result = self.professor1.add_course("Physics")
     #     self.assertEqual(result, "Course Physics already exists")
     #
     # def test_remove_course_success(self):
-    #     self.professor.add_course("Computer Science")
-    #     result = self.professor.remove_course("Computer Science")
+    #     self.professor1.add_course("Computer Science")
+    #     result = self.professor1.remove_course("Computer Science")
     #     self.assertEqual(result, "Course 'Computer Science' removed successfully.")
-    #     self.assertNotIn("Computer Science", self.professor.courses.courses)
+    #     self.assertNotIn("Computer Science", self.professor1.courses.courses)
     #
     # def test_remove_course_not_found(self):
-    #     result = self.professor.remove_course("Biology")
+    #     result = self.professor1.remove_course("Biology")
     #     self.assertEqual(result, "Course 'Biology' not found")
     #
     # def test_view_courses(self):
-    #     self.professor.add_course("Mathematics")
-    #     self.professor.add_course("Physics")
+    #     self.professor1.add_course("Mathematics")
+    #     self.professor1.add_course("Physics")
     #     with patch('builtins.print') as mock_print:
-    #         self.professor.view_courses()
+    #         self.professor1.view_courses()
     #         mock_print.assert_any_call("Your teaching courses:")
     #         mock_print.assert_any_call("- Mathematics")
     #         mock_print.assert_any_call("- Physics")
